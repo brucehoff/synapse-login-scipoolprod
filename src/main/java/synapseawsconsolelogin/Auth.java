5cf97652b2a224e42a39742bb1237c8f0627c119
Khai Do <3697686+zaro0508@users.noreply.github.com>
2020-03-25 22:33:22 -0700

    [SC-64] health check endpoint (#18)
    Setup a health check url endpoint to allow AWS Beanstalk to check
    that the app is up and running.

--- src/main/java/synapseawsconsolelogin/Auth.java
+++ src/main/java/synapseawsconsolelogin/Auth.java
@@ -57,6 +57,7 @@ public class Auth extends HttpServlet {
 
 	private static final String TOKEN_URL = "https://repo-prod.prod.sagebase.org/auth/v1/oauth2/token";
 	private static final String REDIRECT_URI = "/synapse";
+	private static final String HEALTH_URI = "/health";
 	private static final String AWS_CONSOLE_URL = "https://console.aws.amazon.com/servicecatalog";
 	private static final String AWS_SIGN_IN_URL = "https://signin.aws.amazon.com/federation";
 	private static final String USER_CLAIMS_DEFAULT="userid";
@@ -308,6 +309,8 @@ public class Auth extends HttpServlet {
 			
 			resp.setHeader("Location", redirectURL);
 			resp.setStatus(302);
+		}	else if (uri.equals(HEALTH_URI)) {
+			resp.setStatus(200);
 		} else {
 			throw new RuntimeException("Unexpected URI "+req.getRequestURI());
 		}
