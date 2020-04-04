a68dc4d0b8afe25dbe449fbc29d973fc1590f553
bhoff <bruce.hoff@sagebase.org>
2020-04-03 17:29:17 -0700

    SC-107: ensure we connect to the correct region

--- src/test/java/synapseawsconsolelogin/AuthTest.java
+++ src/test/java/synapseawsconsolelogin/AuthTest.java
@@ -1,6 +1,7 @@
 package synapseawsconsolelogin;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
